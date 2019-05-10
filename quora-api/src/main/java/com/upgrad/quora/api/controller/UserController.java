308a29c83bec125acc677c434a5abb0fb35beb9c
Ansar Memon <ansar.memon@hotmail.com>
2019-05-10 18:24:29 +0400

    Added user signin endpoint with user not found exception handler.

--- quora-api/src/main/java/com/upgrad/quora/api/controller/UserController.java
+++ quora-api/src/main/java/com/upgrad/quora/api/controller/UserController.java
@@ -1,15 +1,20 @@
 package com.upgrad.quora.api.controller;
 
+import com.upgrad.quora.api.model.SigninResponse;
 import com.upgrad.quora.api.model.SignupUserRequest;
 import com.upgrad.quora.api.model.SignupUserResponse;
+import com.upgrad.quora.service.entity.UserAuthEntity;
 import com.upgrad.quora.service.entity.UserEntity;
 import com.upgrad.quora.service.business.UserBusinessService;
+import com.upgrad.quora.service.exception.AuthenticationFailedException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.Base64;
 import java.util.UUID;
 
 @RestController
@@ -19,9 +24,26 @@ public class UserController {
   @Autowired
   private UserBusinessService userBusinessService;
 
-  @GetMapping
-  public String getUser() {
-    return "Hello User";
+  @RequestMapping(method = RequestMethod.POST, path = "signin", produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
+  public ResponseEntity<SigninResponse> signin(@RequestHeader("authorization") final String authorization) throws AuthenticationFailedException {
+    byte[] decoded = Base64.getDecoder().decode(authorization.split(" ")[1]);
+    String decodedText = new String(decoded);
+    String[] decodedArray = decodedText.split(":");
+
+
+    UserAuthEntity authEntity = userBusinessService.signin(decodedArray[0], decodedArray[1]);
+
+    SigninResponse signinResponse = new SigninResponse();
+
+    signinResponse.setId(authEntity.getUserId().getUuid());
+    signinResponse.setMessage("SIGNED IN SUCCESSFULLY");
+
+    HttpHeaders headers = new HttpHeaders();
+
+    headers.add("access_token", authEntity.getAccessToken());
+
+
+    return new ResponseEntity<SigninResponse>(signinResponse, headers, HttpStatus.OK );
   }
 
 
@@ -45,7 +67,7 @@ public class UserController {
 
     final UserEntity createdUserEntity = userBusinessService.signup(userEntity);
 
-    SignupUserResponse userResponse = new SignupUserResponse().id(createdUserEntity.getUuid()).status("USER REGISTERED SUCCESSFULLY");
+    SignupUserResponse userResponse = new SignupUserResponse().id(createdUserEntity.getUuid()).status("USER SUCCESSFULLY REGISTERED");
 
     return new ResponseEntity<SignupUserResponse>(userResponse, HttpStatus.CREATED);
 
