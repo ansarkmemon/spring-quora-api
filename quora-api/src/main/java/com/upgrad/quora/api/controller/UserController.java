0459da23bb21a0b57faef3f43a4d30afbb0f24a9
Ansar Memon <ansar.memon@hotmail.com>
2019-05-11 10:40:43 +0400

    Added exception handling for signup endpoint

--- quora-api/src/main/java/com/upgrad/quora/api/controller/UserController.java
+++ quora-api/src/main/java/com/upgrad/quora/api/controller/UserController.java
@@ -7,6 +7,8 @@ import com.upgrad.quora.service.entity.UserAuthEntity;
 import com.upgrad.quora.service.entity.UserEntity;
 import com.upgrad.quora.service.business.UserBusinessService;
 import com.upgrad.quora.service.exception.AuthenticationFailedException;
+import com.upgrad.quora.service.exception.SignUpRestrictedException;
+import org.hibernate.exception.ConstraintViolationException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
@@ -47,29 +49,33 @@ public class UserController {
   }
 
 
+//  Sign up endpoint
+
   @RequestMapping(method = RequestMethod.POST, path = "signup", consumes = MediaType.APPLICATION_JSON_UTF8_VALUE, produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
-  public ResponseEntity<SignupUserResponse> signup(@RequestBody SignupUserRequest signupUserRequest) {
+  public ResponseEntity<SignupUserResponse> signup(@RequestBody SignupUserRequest signupUserRequest) throws SignUpRestrictedException {
 
 
-    final UserEntity userEntity = new UserEntity();
+      final UserEntity userEntity = new UserEntity();
 
-    userEntity.setUuid(UUID.randomUUID().toString());
-    userEntity.setFirstname(signupUserRequest.getFirstName());
-    userEntity.setLastname(signupUserRequest.getLastName());
-    userEntity.setUsername(signupUserRequest.getUserName());
-    userEntity.setEmail(signupUserRequest.getEmailAddress());
-    userEntity.setPassword(signupUserRequest.getPassword());
-    userEntity.setCountry(signupUserRequest.getCountry());
-    userEntity.setAboutme(signupUserRequest.getAboutMe());
-    userEntity.setDob(signupUserRequest.getDob());
-    userEntity.setRole("nonadmin");
-    userEntity.setContactnumber(signupUserRequest.getContactNumber());
+      userEntity.setUuid(UUID.randomUUID().toString());
+      userEntity.setFirstname(signupUserRequest.getFirstName());
+      userEntity.setLastname(signupUserRequest.getLastName());
+      userEntity.setUsername(signupUserRequest.getUserName());
+      userEntity.setEmail(signupUserRequest.getEmailAddress());
+      userEntity.setPassword(signupUserRequest.getPassword());
+      userEntity.setCountry(signupUserRequest.getCountry());
+      userEntity.setAboutme(signupUserRequest.getAboutMe());
+      userEntity.setDob(signupUserRequest.getDob());
+      userEntity.setRole("nonadmin");
+      userEntity.setContactnumber(signupUserRequest.getContactNumber());
 
-    final UserEntity createdUserEntity = userBusinessService.signup(userEntity);
+      final UserEntity createdUserEntity = userBusinessService.signup(userEntity);
 
-    SignupUserResponse userResponse = new SignupUserResponse().id(createdUserEntity.getUuid()).status("USER SUCCESSFULLY REGISTERED");
+      SignupUserResponse userResponse = new SignupUserResponse().id(createdUserEntity.getUuid()).status("USER SUCCESSFULLY REGISTERED");
 
-    return new ResponseEntity<SignupUserResponse>(userResponse, HttpStatus.CREATED);
+      return new ResponseEntity<SignupUserResponse>(userResponse, HttpStatus.CREATED);
+
+
 
   }
 }
