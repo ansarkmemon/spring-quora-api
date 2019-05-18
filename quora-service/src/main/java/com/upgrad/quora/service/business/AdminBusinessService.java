93702cb24a2c21fd6354b948843798a1440b4c5c
dushyanthshenoyr <dushyanthshenoy@icloud.com>
2019-05-18 17:28:41 +0530

    Refactored the code and optimized the imports

--- quora-service/src/main/java/com/upgrad/quora/service/business/AdminBusinessService.java
+++ quora-service/src/main/java/com/upgrad/quora/service/business/AdminBusinessService.java
@@ -3,14 +3,12 @@ package com.upgrad.quora.service.business;
 import com.upgrad.quora.service.dao.AdminDao;
 import com.upgrad.quora.service.entity.UserAuthEntity;
 import com.upgrad.quora.service.entity.UserEntity;
-import com.upgrad.quora.service.exception.AdminAuthorFailedException;
 import com.upgrad.quora.service.exception.AuthorizationFailedException;
 import com.upgrad.quora.service.exception.UserNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
-import java.time.ZonedDateTime;
 
 @Service
 public class AdminBusinessService {
@@ -31,44 +29,13 @@ public class AdminBusinessService {
     }
 
     @Transactional
-    public String deleteUser(final String userid , final String accessToken) throws AuthorizationFailedException,AdminAuthorFailedException, UserNotFoundException {
+    public String deleteUser(String accessToken, String userId) throws AuthorizationFailedException, UserNotFoundException {
 
-        UserEntity userEntity = getUser(userid,accessToken);
-        UserAuthEntity userAuthEntity = userDao.getUserAuthByToken(accessToken);
-
-        if(userAuthEntity.getUserId().getRole().equals("admin")){
-            return userDao.deleteUser(userid);
+        UserEntity userById = userBusinessService.getUserById(userId);
+        if(this.confirmAdmin(accessToken)) {
+            adminDao.deleteUserByUuid(userId);
         }
-        else
-        {
-            throw new AdminAuthorFailedException("ATHR-003","Unauthorized Access, Entered user is not an admin");
-        }
-    }
 
-    @Transactional
-    public UserEntity getUser(final String id , final String authorizedToken) throws AuthorizationFailedException,AdminAuthorFailedException, UserNotFoundException {
-
-        UserAuthEntity userAuth =  userDao.getUserAuthByToken(authorizedToken);
-
-
-        if(userAuth == null)
-        {
-            throw new AdminAuthorFailedException("ATHR-001","User has not signed in");
-        }
-        final ZonedDateTime signOutUserTime = userAuth.getLogoutAt();
-
-        if(signOutUserTime!=null && userAuth!=null)
-        {
-            throw new AuthorizationFailedException("ATHR-002","User is signed out.Sign in first to get user details");
-        }
-        UserEntity user = userDao.getUserById(id);
-
-        if(user!=null)
-        {
-            return user;
-        }
-        else {
-            throw new UserNotFoundException("USR-001", "User with entered uuid does not exist .");
-        }
+        return userId;
     }
 }
