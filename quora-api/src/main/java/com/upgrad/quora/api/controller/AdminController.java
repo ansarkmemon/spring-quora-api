a8fcc3912da3bbadf09495d603cced8a9d9a1819
dushyanthshenoyr <dushyanthshenoy@icloud.com>
2019-05-18 16:51:41 +0530

    optimized the imports

--- quora-api/src/main/java/com/upgrad/quora/api/controller/AdminController.java
+++ quora-api/src/main/java/com/upgrad/quora/api/controller/AdminController.java
@@ -1,21 +1,14 @@
 package com.upgrad.quora.api.controller;
 
-import com.upgrad.quora.api.model.*;
+import com.upgrad.quora.api.model.UserDeleteResponse;
 import com.upgrad.quora.service.business.AdminBusinessService;
-import com.upgrad.quora.service.exception.AdminAuthorFailedException;
 import com.upgrad.quora.service.exception.AuthorizationFailedException;
 import com.upgrad.quora.service.exception.UserNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-
-import java.time.ZonedDateTime;
-import java.util.LinkedList;
-import java.util.List;
-import java.util.UUID;
 
 @RestController
 @RequestMapping("/admin")
@@ -24,7 +17,7 @@ public class AdminController {
     @Autowired
     private AdminBusinessService adminBusinessService;
 
-    @RequestMapping(method = RequestMethod.DELETE , path = "/admin/user/{userId}")
+    @RequestMapping(method = RequestMethod.DELETE , path = "/user/{userId}")
     public ResponseEntity <UserDeleteResponse> deleteUser(@PathVariable("userId") final String userid,
                                                           @RequestHeader("authorization") final String authorization) throws AdminAuthorFailedException,AuthorizationFailedException, UserNotFoundException {
 
